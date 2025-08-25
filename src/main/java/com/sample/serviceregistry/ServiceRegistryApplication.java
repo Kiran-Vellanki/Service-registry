34e8dfe324efd845a3ec600096ed02c5b9d6b2da
Kiran-Vellanki <kiranvellanki10@gmail.com>
2025-08-25 21:41:15 +0530

    Update pom.xml and application properties for Spring Boot and Eureka setup
    - Downgraded Spring Boot version to 3.0.9 to make it compatible with other services
    - Added Spring Cloud version and dependency management for Spring Cloud
    - Included Spring Boot starter web dependency
    - Configured application properties for Eureka server

--- src/main/java/com/sample/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/sample/serviceregistry/ServiceRegistryApplication.java
@@ -2,8 +2,14 @@ package com.sample.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+/**
+ * @author KiranVellanki
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
